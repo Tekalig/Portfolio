8ba82dcddfae2a9b38fe10d30c8084006554e81a
Tekalig <tekahazi06@gmail.com>
2025-02-18 02:46:47 +0300

    add Font Awesome integration and style import to main application file

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
-import { createApp } from 'vue';
+import { createApp } from 'vue'
+import './style.css'
 import App from './App.vue';
 import router from './router';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-createApp(App).use(router).mount('#app');
+library.add(faGithub);
+
+const app = createApp(App);
+app.component('font-awesome-icon', FontAwesomeIcon);
+app.use(router).mount('#app');
