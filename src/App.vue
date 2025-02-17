134a883c5704776383ce6722a8dd6cc2bce2d75e
Tekalig <tekahazi06@gmail.com>
2025-02-18 00:54:46 +0300

    add App.vue with Navbar and Footer components

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app" class="font-sans bg-gray-900 text-white min-h-screen">
+    <Navbar />
+    <router-view />
+    <Footer />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Navbar from './components/Navbar.vue';
+import Footer from './components/Footer.vue';
+
+export default defineComponent({
+  components: { Navbar, Footer },
+});
+</script>
