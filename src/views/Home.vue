8ed0335548b4e1448b0f5ab3bd307d6e7d4f2c4a
Tekalig <tekahazi06@gmail.com>
2025-02-18 01:06:53 +0300

    add Home view component with welcome message and introduction

---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="pt-24 px-6">
+        <h1 class="text-5xl font-bold text-purple-400">Welcome to My Portfolio</h1>
+        <p class="mt-4 text-gray-300">I'm a passionate developer creating amazing web experiences.</p>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'Home',
+});
+</script>
