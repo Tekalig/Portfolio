8eaa88bd010f34a334b5e393a7aca47578895093
Tekalig <tekahazi06@gmail.com>
2025-02-18 00:55:01 +0300

    add Footer component with copyright notice

---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+    <footer class="p-6 bg-gray-800 text-center">
+        <p class="text-purple-400">&copy; 2023 My Portfolio. All rights reserved.</p>
+    </footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'Footer',
+});
+</script>
