36c29d4a7f1641fb7cc1bd0c403b6d80f41f7990
Tekalig <tekahazi06@gmail.com>
2025-02-18 16:28:28 +0300

    refactor App.vue to include Sidebar and ProfileCard components for improved layout

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,29 @@
 <template>
-  <div id="app" class="font-sans bg-gray-900 text-white min-h-screen">
-    <Navbar />
-    <router-view />
-    <Footer />
+  <div id="app" class="font-sans bg-gray-50 text-black min-h-screen flex dark:bg-gray-900 dark:text-white">
+    <!-- Left Sidebar -->
+    <Sidebar class="w-1/5 hidden md:block" />
+
+    <!-- Main Content Area -->
+    <div class="flex-1 flex flex-col items-center justify-center min-h-screen">
+      <router-view />
+    </div>
+
+    <!-- Right Sidebar -->
+    <ProfieCard class="w-1/5 hidden md:block" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import Navbar from './components/Navbar.vue';
 import Footer from './components/Footer.vue';
+import Sidebar from './components/Sidebar.vue';
+import ProfieCard from './components/Profile-card.vue';
 
 export default defineComponent({
-  components: { Navbar, Footer },
+  components: { Footer, Sidebar, ProfieCard },
 });
 </script>
+
+<style scoped>
+/* Add styles if necessary */
+</style>
