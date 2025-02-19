0aa4225eba9012d1c502f685a6430b1ef4503e40
Tekalig <tekahazi06@gmail.com>
2025-02-19 23:29:17 +0300

    refactor App component layout to adjust sidebar widths and add footer

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div id="app" class="font-sans bg-gray-50 text-black min-h-screen flex dark:bg-gray-900 dark:text-white">
+  <div id="app" class="font-sans bg-gray-100 text-black min-h-screen flex dark:bg-gray-900 dark:text-white">
     <!-- Left Sidebar -->
-    <Sidebar class="w-1/5 hidden md:block" />
+    <Sidebar class="w-1/4 hidden md:block" />
 
     <!-- Main Content Area -->
     <div class="flex-1 flex flex-col items-center justify-center min-h-screen">
@@ -9,21 +9,20 @@
     </div>
 
     <!-- Right Sidebar -->
-    <ProfieCard class="w-1/5 hidden md:block" />
+    <ProfileCard class="w-1/4 hidden md:block" />
   </div>
+
+  <!-- Footer -->
+  <Footer class="w-full" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import Footer from './components/Footer.vue';
 import Sidebar from './components/Sidebar.vue';
-import ProfieCard from './components/Profile-card.vue';
+import ProfileCard from './components/Profile-card.vue';
 
 export default defineComponent({
-  components: { Footer, Sidebar, ProfieCard },
+  components: { Footer, Sidebar, ProfileCard },
 });
 </script>
-
-<style scoped>
-/* Add styles if necessary */
-</style>
