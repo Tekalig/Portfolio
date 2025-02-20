4d645444da0748c28624d6cd30a27882f5f8ea42
Tekalig <tekahazi06@gmail.com>
2025-02-20 17:47:08 +0300

    add: add routes for Education and WorkHistory components

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,9 +4,13 @@ import Home from '../views/Home.vue';
 import About from '../views/About.vue';
 import Projects from '../views/Projects.vue';
 import Contact from '../views/Contact.vue';
+import Education from '../views/Education.vue';
+import WorkHistory from '../views/WorkHistory.vue';
 
 const routes: Array<RouteRecordRaw> = [
     { path: '/', component: Home },
+    {path: '/education', component:Education},
+    {path: '/work', component:WorkHistory},
     { path: '/about', component: About },
     { path: '/projects', component: Projects },
     { path: '/contact', component: Contact },
