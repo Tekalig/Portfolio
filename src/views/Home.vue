b1e04d548f63b04a9379ad45ea07993caf6d2cba
Tekalig <tekahazi06@gmail.com>
2025-02-25 12:28:15 +0300

    feat: add gap between sections in Home view for improved layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex flex-col items-center justify-center bg-gray-100 dark:bg-gray-900 px-6"> 
+    <div class="flex flex-col items-center justify-center gap-6 bg-gray-100 dark:bg-gray-900 px-6"> 
         <HeroSection />
         <ServicesSection />
     </div>
